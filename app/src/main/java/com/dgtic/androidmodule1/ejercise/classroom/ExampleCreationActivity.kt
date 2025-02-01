b9b4aca3ae8a93bc55f8989132badadf9a5246af
carlogs <carlogs@hotmail.com>
2025-02-01 11:21:56 -0600

    Pruebas en clase

---
+++ app/src/main/java/com/dgtic/androidmodule1/ejercise/classroom/ExampleCreationActivity.kt
@@ -0,0 +1,24 @@
+package com.dgtic.androidmodule1.ejercise.classroom
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dgtic.androidmodule1.R
+
+class ExampleCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_example_creation)
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
