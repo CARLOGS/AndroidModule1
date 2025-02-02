64a05fbc3d31bac93f6c92d03f39c5e87d873b6d
Lex Santos <chris@Cypher.local>
2025-02-02 00:05:15 -0600

    Ejercicio 1 ChrisSantos

---
+++ app/src/main/java/com/dgtic/androidmodule1/ejercise/home/alexissantos/ActivityOne.kt
@@ -0,0 +1,21 @@
+package com.dgtic.androidmodule1.ejercise.home.alexissantos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dgtic.androidmodule1.R
+
+class ActivityOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_one)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
