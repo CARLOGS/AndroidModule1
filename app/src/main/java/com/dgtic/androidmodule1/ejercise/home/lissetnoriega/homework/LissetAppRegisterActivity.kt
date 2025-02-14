4f504fae625ed927e110475eb233cc48a0d1e14b
LissetW <lissetdakot@gmail.com>
2025-02-13 23:14:36 -0600

    Add logic to validate form

--- app/src/main/java/com/dgtic/androidmodule1/ejercise/home/lissetnoriega/homework/LissetAppRegisterActivity.kt
+++ app/src/main/java/com/dgtic/androidmodule1/ejercise/home/lissetnoriega/homework/LissetAppRegisterActivity.kt
@@ -2,6 +2,9 @@ package com.dgtic.androidmodule1.ejercise.home.lissetnoriega.homework
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -19,6 +22,45 @@ import com.dgtic.androidmodule1.R
 import com.dgtic.androidmodule1.ejercise.home.lissetnoriega.LissetMainActivity
 
 class LissetAppRegisterActivity : AppCompatActivity() {
+    private lateinit var etName: EditText
+    private lateinit var etLastName: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword1: EditText
+    private lateinit var etPassword2: EditText
+    private lateinit var spCountry: Spinner
+    private lateinit var cbConditions: CheckBox
+    private lateinit var btnRegister: Button
+
+    private fun initViews(){
+        etName = findViewById(R.id.etName)
+        etLastName = findViewById(R.id.etLastName)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword1 = findViewById(R.id.etPassword1)
+        etPassword2 = findViewById(R.id.etPassword2)
+        spCountry = findViewById(R.id.spCountry)
+        cbConditions = findViewById(R.id.cbConditions)
+        btnRegister = findViewById(R.id.btnRegister)
+    }
+    private fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun validateFields(){
+        val name = etName.text.toString()
+        val lastName = etLastName.text.toString()
+        val email = etEmail.text.toString()
+        val password1 = etPassword1.text.toString()
+        val password2 = etPassword2.text.toString()
+        val termsAccepted = cbConditions.isChecked
+
+        val isEmailValid = isValidEmail(email)
+        val doPasswordsMatch = password1.isNotEmpty() && (password1 == password2)
+        val fullNameOk = name.isNotEmpty() && lastName.isNotEmpty()
+        val isFormValid = termsAccepted && isEmailValid && doPasswordsMatch && fullNameOk
+
+        btnRegister.isEnabled = isFormValid
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,15 +70,10 @@ class LissetAppRegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val etEmail = findViewById<EditText>(R.id.etEmail)
-        val etPassword1 = findViewById<EditText>(R.id.etPassword1)
-        val etPassword2 = findViewById<EditText>(R.id.etPassword2)
-        val spCountry = findViewById<Spinner>(R.id.spCountry)
-        val cbConditions = findViewById<CheckBox>(R.id.cbConditions)
-        val btnRegister = findViewById<Button>(R.id.btnRegister)
         val ibBack = findViewById<ImageButton>(R.id.ibBack)
         val ibHome = findViewById<ImageButton>(R.id.ibHome)
+        initViews()
+        btnRegister.isEnabled = false
 
         //Spinner
         val data = arrayListOf("Selecciona un país", "México", "Argentina", "Colombia", "Ecuador", "Dinamarca", "España")
@@ -72,11 +109,10 @@ class LissetAppRegisterActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         }
-
+        validateFields()
         ibBack.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
-
         ibHome.setOnClickListener {
             val homeIntent = Intent(this, LissetMainActivity::class.java)
             startActivity(homeIntent)
